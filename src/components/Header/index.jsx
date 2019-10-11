59ca20141d4335b46b563ca92b13e03fbac55aa6
Daniel <hola@dapino.co>
2019-10-11 07:53:08 -0500

    feat: create and style hader nav bar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
 import '../../assets/styles/components/Header.scss';
 import iconSearch from '../../assets/static/search-icon.png'
+import Logo from "../utils/Logo";
 
 const Header = () => (
     <header className="header">
+        <Logo/>
+        <nav className="navBar">
+            <ul className="nav">
+                <li className="nav__item">
+                    <Link to="/">Past Trials</Link>
+                </li>
+                <li className="nav__item">
+                    <Link className="nav__item" to="/">How it Works</Link>
+                </li>
+                <li className="nav__item">
+                    <Link className="nav__item" to="login">Log In / Sign Up</Link>
+                </li>
+                <li className="nav__item">
+                    <span className="icon--search">
+                        <img src={iconSearch} alt=""/>
+                    </span>
+                </li>
+            </ul>
+        </nav>
 
-            <nav className="navBar">
-                <ul className="nav">
-                    <li className="nav__item"><a href="#">Past Trials</a></li>
-                    <li className="nav__item"><a href="#">How it Works</a></li>
-                    <li className="nav__item"><a href="#">Log In / Sign Up</a></li>
-                </ul>
-            </nav>
-            <span className="icon icon--search">
-                <img src={iconSearch} alt=""/>
-            </span>
     </header>
 );
 
